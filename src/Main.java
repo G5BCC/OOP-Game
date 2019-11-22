738f5f1b69265859754c881c2334c93a605599fe
horchulhack <vpemfh7@gmail.com>
2019-11-22 16:45:30 -0300

    Refatoração de código

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
 import formulario.Formulario;
-import jogo.Territorio;
+import jogo.Dados;
 
 public class Main {
     public static void main(String[] args) {
         Formulario form = new Formulario("Formulário");
+        String caminho = "/home/pedrohorchulhack/Documents/Repositories/ProjetoPOO/saves/Pedasd.trt";
+
+        try {
+            Dados dados = form.abrir(caminho);
+            System.out.println(dados.getLargura());
+            System.out.println(dados.getAltura());
+            System.out.println(dados.getNome());
+            System.out.println(dados.getPontuacaoMax());
+            System.out.println(dados.getQuantInimigos());
+            System.out.println(dados.getRitmoJogo());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
